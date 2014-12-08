c63de05336709378cc3c2f5a526803d5627fb756
czhang81 <chizhang1@ufl.edu>
2014-12-08 12:30:16 -0500

    homework

--- plot1.R
+++ plot1.R
@@ -3,7 +3,7 @@ mydata$Date<-as.Date(mydata$Date, format="%d/%m/%Y")
 datatrue<-subset(mydata,Date>="2007-02-01"& Date <= "2007-02-02")
 rm(mydata)
 datatrue$Date <- strptime(paste(datatrue$Date,datatrue$Time), "%d/%m/%Y %H:%M:%S")
-hist(datatrue$Global_active_power,main="Global Active Power",xlab="Global Active Power(kilowatts),"
+hist(datatrue$Global_active_power,main="Global Active Power",xlab="Global Active Power(kilowatts)",
      ylab="Frequency",col="Red")
 dev.copy(png,file="plot1.png")
 dev.off()
